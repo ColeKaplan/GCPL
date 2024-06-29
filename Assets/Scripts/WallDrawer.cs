43afbd8bbcf641186c1b5dc55def09d9fba9f705
ColeKaplan <colegkaplan@gmail.com>
2024-06-29 15:07:03 -0400

    Tilemaps

--- Assets/Scripts/WallDrawer.cs
+++ Assets/Scripts/WallDrawer.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemap;
 
 public class WallDrawer : MonoBehaviour
 {
+    public Tilemap tilemap;
+    public Tile wall;
+    public List<GameObject> walls;
+    public Transform tileGridUI;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject UITile = new GameObject("UI Tile");
     }
 
     // Update is called once per frame
