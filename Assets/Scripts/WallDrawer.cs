7e802b064cd26368ed3bfbe26b2e1a1680243d18
ColeKaplan <colegkaplan@gmail.com>
2024-06-29 20:45:33 -0400

    fully working wall connections

--- Assets/Scripts/WallDrawer.cs
+++ Assets/Scripts/WallDrawer.cs
@@ -6,7 +6,9 @@ using UnityEngine.Tilemaps;
 public class WallDrawer : MonoBehaviour
 {
     public Tilemap tilemap;
-    public Tile wall;
+    public Tile[] walls; // Ordering of this matters a lot. Up = 1, Down = 2, Left = 4, Right = 8. Add them up and use that position in the array
+    
+    
 
 
     // Start is called before the first frame update
@@ -20,7 +22,50 @@ public class WallDrawer : MonoBehaviour
     {
         if(Input.GetMouseButton(0)) {
             Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            tilemap.SetTile(tilemap.WorldToCell(position), wall);
+            Vector3Int tilemapPosition = tilemap.WorldToCell(position);
+            int wallNum = calculateWallSurroundings(tilemapPosition);
+            tilemap.SetTile(tilemapPosition, walls[wallNum]);
+            updateNeighbors(tilemapPosition);
+        }
+    }
+
+    private int calculateWallSurroundings(Vector3Int pos){
+        int wallNum = 0;
+        if(tilemap.GetTile(pos + new Vector3Int(0,1,0)) != null){
+            wallNum += 1;
+        }
+        if(tilemap.GetTile(pos + new Vector3Int(0,-1,0)) != null){
+            wallNum += 2;
+        }
+        if(tilemap.GetTile(pos + new Vector3Int(-1,0,0)) != null){
+            wallNum += 4;
+        }
+        if(tilemap.GetTile(pos + new Vector3Int(1,0,0)) != null){
+            wallNum += 8;
+        }
+        return wallNum;
+    }
+
+    private void updateNeighbors(Vector3Int pos){
+
+        Vector3Int up = pos + new Vector3Int(0,1,0);
+        if(tilemap.GetTile(up) != null){
+            tilemap.SetTile(up, walls[calculateWallSurroundings(up)]);
+        }
+
+        Vector3Int down = pos + new Vector3Int(0,-1,0);
+        if(tilemap.GetTile(down) != null){
+            tilemap.SetTile(down, walls[calculateWallSurroundings(down)]);
+        }
+
+        Vector3Int left = pos + new Vector3Int(-1,0,0);
+        if(tilemap.GetTile(left) != null){
+            tilemap.SetTile(left, walls[calculateWallSurroundings(left)]);
+        }
+
+        Vector3Int right = pos + new Vector3Int(1,0,0);
+        if(tilemap.GetTile(right) != null){
+            tilemap.SetTile(right, walls[calculateWallSurroundings(right)]);
         }
     }
 }
