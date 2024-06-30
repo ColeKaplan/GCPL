c83db9273ba9bffd497f309d61abf07fb51352cd
ColeKaplan <colegkaplan@gmail.com>
2024-06-30 10:58:55 -0400

    leaderboard stuff

--- Assets/AstarPathfindingProject/Core/AstarPath.cs
+++ Assets/AstarPathfindingProject/Core/AstarPath.cs
@@ -831,11 +831,12 @@ public class AstarPath : VersionedMonoBehaviour {
 
 			if (logPathResults == PathLog.InGame) {
 				inGameDebugPath = debug;
-			} else if (path.error) {
-				Debug.LogWarning(debug);
-			} else {
-				Debug.Log(debug);
 			}
+			// } else if (path.error) {
+			// 	Debug.LogWarning(debug);
+			// } else {
+			// 	Debug.Log(debug);
+			// }
 		}
 	}
 
@@ -1780,9 +1781,9 @@ public class AstarPath : VersionedMonoBehaviour {
 
 		System.GC.Collect();
 
-		if (logPathResults != PathLog.None && logPathResults != PathLog.OnlyErrors) {
-			Debug.Log("Scanning - Process took "+(lastScanTime*1000).ToString("0")+" ms to complete");
-		}
+		// if (logPathResults != PathLog.None && logPathResults != PathLog.OnlyErrors) {
+		// 	Debug.Log("Scanning - Process took "+(lastScanTime*1000).ToString("0")+" ms to complete");
+		// }
 	}
 
 	IEnumerable<Progress> ScanGraph (NavGraph graph) {
