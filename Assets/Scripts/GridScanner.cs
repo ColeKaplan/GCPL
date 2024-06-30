11f1cf956845cd0523d3ae94f5358c7fd19121ff
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2024-06-29 20:48:37 -0400

    fixed path finding

--- Assets/Scripts/GridScanner.cs
+++ Assets/Scripts/GridScanner.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using Pathfinding; 
 public class GridScanner : MonoBehaviour
 {
+
+    private float deltaTime;
     void Start()
     {
-        
+        deltaTime = 0.0f;
         if (AstarPath.active != null)
         {
            
@@ -15,5 +17,15 @@ public class GridScanner : MonoBehaviour
         {
             Debug.LogError("AstarPath object is not active or initialized.");
         }
+    }
+
+    void Update() {
+        if (deltaTime >= 30) {
+            AstarPath.active.Scan();
+            deltaTime = 0.0f;
+        } else {
+            deltaTime += Time.fixedDeltaTime;
+        }
+        
     }
 }
